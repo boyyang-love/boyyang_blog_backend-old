2fde841df44bc8c66846cfb3ca4eb26e30540f7a
yang <boyyang-love@outlook.com>
2022-04-05 16:11:43 +0800

    修改跨域中间件

--- middleware/cors.go
+++ middleware/cors.go
@@ -1,7 +1,7 @@
 /**
  * @Author: boyyang
  * @Date: 2022-04-04 23:41:57
- * @LastEditTime: 2022-04-04 23:45:06
+ * @LastEditTime: 2022-04-05 16:11:17
  * @LastEditors: boyyang
  * @Description:
  * @FilePath: \blog\middleware\cors.go
@@ -21,6 +21,8 @@ func Cors() gin.HandlerFunc {
 		method := c.Request.Method
 		origin := c.Request.Header.Get("Origin") //请求头部
 		if origin != "" {
+			//接收客户端发送的origin （重要！）
+			c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
 			c.Header("Access-Control-Allow-Origin", "*") // 可将将 * 替换为指定的域名
 			c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
 			c.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
